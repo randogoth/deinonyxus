f48f0615b6b0d3b8823153b235ae322c1c2787e0
randogoth <kflux@posteo.de>
2025-11-22 10:49:43 +0200

    added grub2 theme, removed docker and code

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -5,6 +5,5 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+sudo chmod 755 /usr/local/sbin/bls-add-bluefin.sh
+sudo systemctl daemon-reload
