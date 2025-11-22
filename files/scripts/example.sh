a16562e24d1200959d775b743bb9613ef16d114e
Flux <kflux@posteo.de>
2025-11-22 14:46:08 +0200

    Init (#2)
    * added grub2 theme, removed docker and code
    
    * removed grub stuff
    
    * removed docker and code, added flatpaks

--- files/scripts/example.sh
+++ files/scripts/example.sh
@@ -4,7 +4,3 @@
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
 set -oue pipefail
-
-# Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
